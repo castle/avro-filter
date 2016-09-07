7e5930d14e88389143b9d60e1804c758bc891e3a
Sebastian Wallin <sebastian.wallin@gmail.com>
2016-09-07 16:08:22 +0200

    Handle multiple input files and/or directories

--- build.sbt
+++ build.sbt
@@ -9,6 +9,9 @@ scalacOptions += "-deprecation"
 libraryDependencies += "org.apache.avro" % "avro" % "1.8.1"
 libraryDependencies += "com.github.scopt" %% "scopt" % "3.5.0"
 
+libraryDependencies += "org.apache.hadoop" % "hadoop-common" % "2.7.3"
+libraryDependencies += "org.apache.hadoop" % "hadoop-aws" % "2.7.3"
+
 resolvers += Resolver.sonatypeRepo("public")
 
 assemblyJarName in assembly := "avro-filter.jar"
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -11,8 +11,16 @@ import org.apache.avro.file.CodecFactory
 
 import java.io.File
 import java.io.FileInputStream
+import java.io.InputStream
 
-case class Config(out: File = new File("."), schema: File = null, files: Seq[File] = Seq(), filter: Map[String,String] = Map())
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.fs.FileStatus
+import org.apache.hadoop.fs.FileSystem
+import org.apache.hadoop.fs.Path
+
+import scala.collection.mutable.ListBuffer
+
+case class Config(out: File = new File("."), schema: File = null, files: Seq[String] = Seq(), filter: Map[String,String] = Map())
 
 object AvroFilter {
   val parser = new scopt.OptionParser[Config]("avro-filter") {
@@ -31,61 +39,95 @@ object AvroFilter {
 
     help("help").text("prints this usage text")
 
-    arg[File]("<files>...").unbounded().action( (x, c) =>
+    arg[String]("<files>...").unbounded().action( (x, c) =>
       c.copy(files = c.files :+ x) ).text("input file(s)")
   }
 
+  def getFiles(fileOrDirName:String) : List[Path] = {
+    val path:Path = new Path(fileOrDirName);
+    val fs:FileSystem = path.getFileSystem(new Configuration())
+
+    val fileList = new ListBuffer[Path]()
+
+    if (fs.isFile(path)) {
+      fileList += path
+    }
+    else if (fs.getFileStatus(path).isDir()) {
+      for (status <- fs.listStatus(path)) {
+        if(!status.isDir()) {
+          fileList += status.getPath()
+        }
+      }
+    }
+    return fileList.toList
+  }
+
   def main(args: Array[String]) = {
+
+
     val options = parser.parse(args, Config())
 
     if (options == None) {
       System.exit(0)
     }
 
-    val file = options.get.files.head
-    if ( !file.canRead() ) {
-      println("File cannot be opened for reading")
-      System.exit(1)
+
+    var inFiles = List[Path]()
+    for (path <- options.get.files) {
+      inFiles = inFiles ++ getFiles(path)
     }
 
-    var reader :GenericDatumReader[GenericRecord] = null
     var schema :Schema = null
     if (options.get.schema != null) {
       schema = (new Schema.Parser()).parse(options.get.schema)
-      reader = new GenericDatumReader(schema)
-    } else {
-      reader = new GenericDatumReader()
+      println("Using schema: " + schema.getFullName())
     }
-
-    val fileStream = new FileInputStream(file)
-    val fileReader = new DataFileStream(fileStream, reader)
-
-    if (schema == null) {
-      schema = fileReader.getSchema()
-    }
-
-    println("Using schema: " + schema.getFullName())
-    val codecFactory = CodecFactory.deflateCodec(9)
 
     val writer :DataFileWriter[GenericRecord] = new DataFileWriter(new GenericDatumWriter())
+    val codecFactory = CodecFactory.deflateCodec(9)
     writer.setCodec(codecFactory)
 
     val outFile = options.get.out
-    writer.create(schema, outFile)
+    var isOpen = false
 
     var writeCount:Int = 0
     var totalCount:Int = 0
 
-    while (fileReader.hasNext()) {
-      breakable {
-        totalCount += 1
-        val data = fileReader.next()
-        options.get.filter.foreach { case(k, v) =>
-          if (data.get(k).toString != v) break
-        }
-        writeCount += 1
-        writer.append(data)
+    for (inFile:Path <- inFiles) {
+      var reader:GenericDatumReader[GenericRecord] = null
+      val input:InputStream = inFile.getFileSystem(new Configuration()).open(inFile)
+
+      if (schema != null) {
+        reader = new GenericDatumReader(schema)
+      } else {
+        reader = new GenericDatumReader()
       }
+
+      val fileReader = new DataFileStream(input, reader)
+
+      if (schema == null) {
+        schema = fileReader.getSchema()
+      }
+
+      if (!isOpen) {
+        writer.create(schema, outFile)
+        isOpen = true
+      }
+
+      while (fileReader.hasNext()) {
+        breakable {
+          totalCount += 1
+          val data = fileReader.next()
+          options.get.filter.foreach { case(k, v) =>
+            val value = data.get(k)
+            if (value == null || value.toString != v) break
+          }
+          writeCount += 1
+          writer.append(data)
+        }
+      }
+
+      fileReader.close()
     }
 
     println(s"Wrote $writeCount of total $totalCount records")
